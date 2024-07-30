6e960d6152082d0148e938adc475bfd3148aed19
Bram Esposito <bram@bramesposito.com>
2024-07-30 12:44:20 +0200

    implement a first version of the Github action

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# npm-outdated-action
-Github Action to check for outdated NPM packages
+# Check for outdated NPM packages in a Github Action 
+Run this github action to check for outdated NPM packages in your repository. 
+
+### Example
+
+``` yml
+name: Check for outdated packages
+
+on: [push]
+
+jobs:
+  check_packages:
+    runs-on: ubuntu-latest
+    name: A job to test for outdated packages
+    steps:
+    - name: Test the released step
+      id: check_packages
+      uses: bramesposito/npm-outdated-action@v1
+  
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+const core = require('@actions/core');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec)
+
+async function getOutdated () {
+
+    try {
+        const path = core.getInput('path');
+        const output = await exec('npm outdated --json || true')
+
+        const excludeList_data = core.getInput('exclude-list');
+        const excludeList = excludeList_data.split(",").map(item => item.trim());
+        const o_data = JSON.parse(output.stdout.trim());
+
+        const outdated = Object.entries(o_data).map(([name, { current, wanted, latest }]) => {
+            if (excludeList.includes(name)) {
+                return;
+            }
+            return {
+                name,
+                current,
+                wanted,
+                latest
+            }
+        });
+        if (outdated.length > 0) {
+            core.setFailed("These packages are outdated: " + JSON.stringify(outdated));
+        } else {
+            console.log("Everything is up to date. Enjoy!");
+        }
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+getOutdated();
